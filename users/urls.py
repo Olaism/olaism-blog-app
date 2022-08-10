78612094e67c095b10aabc7e216d3dca72605e62
olaism <olaism01@gmail.com>
2022-08-10 13:40:28 -0700

    Added profile functionalities

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from .views import SignupView
+from .views import (
+    SignupView,
+    Profile
+)
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
+    path('profile/', Profile.as_view(), name='my_profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.contrib.auth import get_user_model
+from django.views.generic.edit import (
+    CreateView,
+    UpdateView
+)
 
 from .forms import CustomUserCreationForm
 
@@ -8,3 +12,12 @@ class SignupView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+class Profile(UpdateView):
+    model = get_user_model()
+    fields = ("first_name", 'last_name', 'email',)
+    template_name = 'profile.html'
+    success_url = reverse_lazy('my_profile')
+
+    def get_object(self):
+        return self.request.user
