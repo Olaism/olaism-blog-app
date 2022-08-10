11e0b8a1d438341d2191fc0a449147c5ed1af061
olaism <olaism01@gmail.com>
2022-08-10 05:00:27 -0700

    Debugged authentication error on password reset

--- config/settings.py
+++ config/settings.py
@@ -145,3 +145,5 @@ LOGOUT_REDIRECT_URL = 'login'
 # CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
